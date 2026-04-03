ed013a51335370f871aa22051c6d0757881eba69
n3v4da.zip <122791175+Gabriel-A-Ferretto@users.noreply.github.com>
2026-04-02 23:42:55 -0300

    up

---
+++ ADA-tech/Python/decisão.py
@@ -0,0 +1,7 @@
+idade =  int(input('Qual é sua idade: '))
+
+if idade == 18:
+    print('Pode tomar cachaça avontade')
+else:
+    print('Se-fudeu')
+    
---
+++ ADA-tech/Python/decisão_cachaça.py
@@ -0,0 +1,8 @@
+idade = int(input('Digite sua idade: '))
+
+if idade == 18:
+    print('Pode tomar cahaça avontade')
+elif idade > 18:
+    print('Ja pode se esbaldar de 51')
+else:
+    print('se-fudeu'.upper())
