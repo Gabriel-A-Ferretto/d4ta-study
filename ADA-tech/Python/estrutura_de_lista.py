9a20adcb0a71393bf58373a46d9882984236b9bf
n3v4da.zip <122791175+Gabriel-A-Ferretto@users.noreply.github.com>
2026-04-12 19:53:39 -0300

    up

---
+++ ADA-tech/Python/estrutura_de_lista.py
@@ -0,0 +1,27 @@
+# LISTAS
+
+notas = [7.9, 9.7, 8.2]
+
+listaVazia = []
+
+listaMetodo = list() # list() é um metodo para criar lista 
+
+lista = [23, 'Gabriel', 'SP']
+
+lista_de_listas = [10,[1,2,3]] #lista dentro de outra lista
+
+print(notas[0],'\n',notas[1]) # pegando valor 1 e 2 dentro da lista
+
+print(lista[1])
+
+
+print(lista[-1])
+
+#SLICE
+
+
+listaN = [10, 50, 55, 60, 75, 80, 85, 90, 95, 100 ]
+
+
+print(listaN[0:3])
+
